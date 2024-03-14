8c6a0bb9dbba152b801f655beb119c5bfcf2e346
Archit Vale <152762281+architvale@users.noreply.github.com>
2024-03-14 23:13:42 +0530

    RanNumber.java

--- RanNumber.java
+++ RanNumber.java
@@ -1,10 +1,6 @@
-/*
- Build a program that generates a random number and challenges the user to guess it. The program should prompt the user to input their guess , 
- compare it to the generated number,and provide feedback if the guess is too high or too low. It should continue until the user correctly 
- guesses the number and then display the number of attempts it took to win the game.
- */
-
-
+/* Build a program that generates a random number and challenges the user to guess it. The program should prompt the user to input their guess,
+compare it to the generated number,and provide feedback if the guess is too high or too low. It should continue until the user correctly guesses the number and 
+then display the number of attempts it took to win the game.*/
 
 import java.util.Random;
 import java.util.Scanner;
